3585078e3498ff491dfe01d8657a9481f7226e32
Wanderluzter <leobotelho27@outlook.com>
2024-09-18 18:48:07 -0300

    Created venv for python backend. Imported flask co rs and axios. Coded a bit on App.tsx.

--- friendly/src/App.tsx
+++ friendly/src/App.tsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [array,setArray] = useState([]);
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://127.0.0.1:8080/api/users");
+    setArray(response.data.users);
+  };
+
+  useEffect(() => {
+    fetchAPI()
+  },[]);
 
   return (
     <>
+    <h1>
+      <p>
+        {
+          array.map((user, index) => (
+            <div key={index}>
+            <span>{user}</span>
+            <br />
+            </div>
+          ))
+        }
+      </p>
+    </h1>
     </>
   )
 }
