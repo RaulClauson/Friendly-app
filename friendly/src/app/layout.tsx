6da55ca3c39c11f569e460e7d003fb29c510aea2
Raul <rclauson141@gmail.com>
2024-09-27 18:11:03 -0300

    Tela de carregamento

--- friendly/src/app/layout.tsx
+++ friendly/src/app/layout.tsx
@@ -1,27 +1,40 @@
-import type { Metadata } from "next";
-import "./globals.css";
+"use client";
+import { useState, useEffect, useRef } from "react";
 import Menu from "./Components/Menu/Menu";
-import styles from "./page.module.css";
 import Rodape from "./Components/Rodape/Rodape";
-
-export const metadata: Metadata = {
-  title: "Friendly",
-  description: "Seu melhor amigo virtual.",
-};
+import Loading from "./Components/Loading/Loading";
+import styles from "./page.module.css";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Simulate a network request or some loading task
+    const timer = setTimeout(() => {
+      setLoading(false); // Set loading to false after a delay (or when data finishes loading)
+    }, 2000); // You can adjust the time or trigger this based on data fetching
+
+    return () => clearTimeout(timer); // Clean up the timer
+  }, []);
+
   return (
     <html lang="pt-br">
       <body>
-        <Menu></Menu>
-        <main className={styles.main}>
-          {children}
-          <Rodape></Rodape>
-        </main>
+        {loading ? (
+          <Loading /> // Show the loading animation while loading is true
+        ) : (
+          <>
+            <Menu />
+            <main className={styles.main}>
+              {children}
+              <Rodape />
+            </main>
+          </>
+        )}
       </body>
     </html>
   );
